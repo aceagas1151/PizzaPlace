e8b6c74f04b3bd601f07106a25d60186817e4ca4
AceAgas1151 <acmagas19@gmail.com>
2025-05-18 20:13:45 +0800

    Fix special character found on sample data pizza_types.csv Add CORS Update launch settings to http

--- PizzaPlaceProject/Program.cs
+++ PizzaPlaceProject/Program.cs
@@ -12,18 +12,27 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+
+app.UseCors("AllowFrontend");
+
 
 app.MapControllers();
 
