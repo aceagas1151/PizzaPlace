ff92842a495e7addc8b2588830f090b7fbdff9bc
AceAgas1151 <acmagas19@gmail.com>
2025-05-18 20:23:53 +0800

    Add Json Serialization ReferenceHandler

--- PizzaPlaceProject/Program.cs
+++ PizzaPlaceProject/Program.cs
@@ -7,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PizzaPlaceDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+}); ;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
