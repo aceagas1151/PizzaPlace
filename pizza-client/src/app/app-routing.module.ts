9ca7155393270e967e69d16b48de5baaf1e09ca8
AceAgas1151 <acmagas19@gmail.com>
2025-05-18 22:56:22 +0800

    Add routing to Order Component

--- pizza-client/src/app/app-routing.module.ts
+++ pizza-client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OrdersComponent } from './components/orders/orders.component';
 import { PizzaTypeListComponent } from "./components/pizza-type-list/pizza-type-list.component";
 
 const routes: Routes = [
-  { path: 'pizza-types', component: PizzaTypeListComponent },
-  { path: '', redirectTo: '/pizza-types', pathMatch: 'full' }
+  { path: 'orders', component: OrdersComponent },
+  { path: 'pizzas', component: PizzaTypeListComponent },
+  { path: '', redirectTo: '/orders', pathMatch: 'full' }, // default
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- pizza-client/src/app/app.component.ts
+++ pizza-client/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // ✅ Add this
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterModule], // ✅ Add RouterModule here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'pizza-client';
-}
+export class AppComponent {}
